8ea334daffb3b5c79111e2487599b23639fb55dd
master yu <64240764+maste-ryu@users.noreply.github.com>
2021-07-07 06:59:42 +0800

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -171,11 +171,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
       <PageHead site={site} />
 
       <Head>
-        <meta property='og:title' content={title} />
-        <meta property='og:site_name' content={site.name} />
 
-        <meta name='twitter:title' content={title} />
-        <meta property='twitter:domain' content={site.domain} />
 
         {config.twitter && (
           <meta name='twitter:creator' content={`@${config.twitter}`} />
@@ -184,16 +180,13 @@ export const NotionPage: React.FC<types.PageProps> = ({
         {socialDescription && (
           <>
             <meta name='description' content={socialDescription} />
-            <meta property='og:description' content={socialDescription} />
-            <meta name='twitter:description' content={socialDescription} />
+
           </>
         )}
 
         {socialImage ? (
           <>
-            <meta name='twitter:card' content='summary_large_image' />
-            <meta name='twitter:image' content={socialImage} />
-            <meta property='og:image' content={socialImage} />
+
           </>
         ) : (
           <meta name='twitter:card' content='summary' />
@@ -202,8 +195,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         {canonicalPageUrl && (
           <>
             <link rel='canonical' href={canonicalPageUrl} />
-            <meta property='og:url' content={canonicalPageUrl} />
-            <meta property='twitter:url' content={canonicalPageUrl} />
+
           </>
         )}
 
@@ -276,7 +268,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         }
       />
 
-      <GitHubShareButton />
+
     </TwitterContextProvider>
   )
 }
