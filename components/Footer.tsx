b2403ed972fbf3bfad6caf2724e3d3f6f3c4dc0c
master yu <64240764+maste-ryu@users.noreply.github.com>
2021-07-07 08:51:43 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,45 +26,7 @@ export const Footer: React.FC<{
 
   return (
     
-<button class="js-back-to-top back-to-top" title="back_top">&#65085;</button>
 
-<script src="./jquery.min.js"></script>
-
-<script>
-
-$(function () {
-
-var $win = $(window);
-
-var $backToTop = $('.js-back-to-top');
-
-// 當用戶滾動到離頂部100像素時，展示回到頂部按鈕
-
-$win.scroll(function () {
-
-if ($win.scrollTop() > 100) {
-
-$backToTop.show();
-
-} else {
-
-$backToTop.hide();
-
-}
-
-});
-
-// 當用戶點擊按鈕時，通過動畫效果返回頭部
-
-$backToTop.click(function () {
-
-$('html, body').animate({scrollTop: 0}, 200);
-
-});
-
-});
-
-</script>
 
     
     
